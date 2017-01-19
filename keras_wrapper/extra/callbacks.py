76419bbb1dfa19130d6c1aa7c15dc53598ac3958
MarcBS <marc.bolanos@ub.edu>
2017-01-19 17:29:22 +0100

    Requirements updated

--- keras_wrapper/extra/callbacks.py
+++ keras_wrapper/extra/callbacks.py
@@ -42,6 +42,8 @@ class PrintPerformanceMetricOnEpochEnd(KerasCallback):
                  start_eval_on_epoch=0, write_type='list', sampling_type='max_likelihood',
                  out_pred_idx=None, early_stop=False, patience=5, stop_metric='Bleu-4', verbose=1):
         """
+            DEPRECATED!
+
             :param model: model to evaluate
             :param dataset: instance of the class Dataset in keras_wrapper.dataset
             :param gt_id: identifier in the Dataset instance of the output data about to evaluate
@@ -111,7 +113,7 @@ class PrintPerformanceMetricOnEpochEnd(KerasCallback):
 
             if self.beam_search:
                 params_prediction.update(checkDefaultParamsBeamSearch(self.extra_vars))
-                predictions = self.model_to_eval.BeamSearchNet(self.ds, params_prediction)[s]
+                predictions = self.model_to_eval.predictBeamSearchNet(self.ds, params_prediction)[s]
             else:
                 # Convert predictions
                 postprocess_fun = None
@@ -206,6 +208,8 @@ class PrintPerformanceMetricEachNUpdates(KerasCallback):
                  each_n_updates=10000, start_eval_on_epoch=0, write_type='list', sampling_type='max_likelihood',
                  out_pred_idx=None, early_stop=False, patience=5, stop_metric='Bleu-4', verbose=1):
         """
+            DEPRECATED!
+
             :param model: model to evaluate
             :param dataset: instance of the class Dataset in keras_wrapper.dataset
             :param gt_id: identifier in the Dataset instance of the output data about to evaluate
@@ -274,7 +278,7 @@ class PrintPerformanceMetricEachNUpdates(KerasCallback):
 
             if self.beam_search:
                 params_prediction.update(checkDefaultParamsBeamSearch(self.extra_vars))
-                predictions = self.model_to_eval.BeamSearchNet(self.ds, params_prediction)[s]
+                predictions = self.model_to_eval.predictBeamSearchNet(self.ds, params_prediction)[s]
             else:
                 # Convert predictions
                 postprocess_fun = None
@@ -357,6 +361,235 @@ class PrintPerformanceMetricEachNUpdates(KerasCallback):
                         if self.verbose > 0:
                             logging.info("Update %d: early stopping. Best %s value found at update %d: %.4f" %
                                          (self.cum_update, self.stop_metric, self.best_update, self.best_score))
+                            self.model.stop_training = True
+                    self.wait += 1
+                    if self.verbose > 0:
+                        logging.info('----bad counter: %d/%d' % (self.wait, self.patience))
+
+
+class PrintPerformanceMetricOnEpochEndOrEachNUpdates(KerasCallback):
+
+    def __init__(self, model, dataset, gt_id, metric_name, set_name, batch_size, each_n_epochs=1,
+                 extra_vars=None,
+                 is_text=False, index2word_y=None, input_text_id=None, index2word_x=None,
+                 sampling='max_likelihood',
+                 beam_search=False, write_samples=False, save_path='logs/performance.',
+                 reload_epoch=0,
+                 eval_on_epochs=True, start_eval_on_epoch=0, is_3DLabel=False,
+                 write_type='list', sampling_type='max_likelihood',
+                 out_pred_idx=None, early_stop=False, patience=5, stop_metric='Bleu-4', verbose=1):
+        """
+            :param model: model to evaluate
+            :param dataset: instance of the class Dataset in keras_wrapper.dataset
+            :param gt_id: identifier in the Dataset instance of the output data about to evaluate
+            :param metric_name: name of the performance metric
+            :param set_name: name of the set split that will be evaluated
+            :param batch_size: batch size used during sampling
+            :param each_n_epochs: sampling each this number of epochs
+            :param extra_vars: dictionary of extra variables
+            :param is_text: defines if the predicted info is of type text (in that case the data will be converted from values into a textual representation)
+            :param is_3DLabel: defines if the predicted info is of type 3DLabels
+            :param index2word_y: mapping from the indices to words (only needed if is_text==True)
+            :param sampling: sampling mechanism used (only used if is_text==True)
+            :param write_samples: flag for indicating if we want to write the predicted data in a text file
+            :param save_path: path to dumb the logs
+            :param eval_on_epochs: Eval each epochs or updates
+            :param start_eval_on_epoch: only starts evaluating model if a given epoch has been reached
+            :param write_type: method used for writing predictions
+            :param sampling_type: type of sampling used (multinomial or max_likelihood)
+            :param out_pred_idx: index of the output prediction used for evaluation (only applicable if model has more than one output, else set to None)
+            :param verbose: verbosity level; by default 1
+        """
+        self.model_to_eval = model
+        self.ds = dataset
+        self.gt_id = gt_id
+        self.input_text_id = input_text_id
+        self.index2word_x = index2word_x
+        self.index2word_y = index2word_y
+        self.is_text = is_text
+        self.is_3DLabel = is_3DLabel
+        self.sampling = sampling
+        self.beam_search = beam_search
+        self.metric_name = metric_name
+        self.set_name = set_name
+        self.batch_size = batch_size
+        self.each_n_epochs = each_n_epochs
+        self.extra_vars = extra_vars
+        self.save_path = save_path
+        self.eval_on_epochs = eval_on_epochs
+        self.start_eval_on_epoch = start_eval_on_epoch
+        self.write_type = write_type
+        self.sampling_type = sampling_type
+        self.write_samples = write_samples
+        self.out_pred_idx = out_pred_idx
+        self.early_stop = early_stop
+        self.patience = patience
+        self.stop_metric = stop_metric
+        self.best_score = -1
+        self.best_epoch = -1
+        self.wait = 0
+        self.verbose = verbose
+        self.cum_update = 0
+        self.epoch = reload_epoch
+        super(PrintPerformanceMetricOnEpochEndOrEachNUpdates, self).__init__()
+
+    def on_epoch_end(self, epoch, logs={}):
+        """
+        On epoch end, sample and evaluate on the specified datasets.
+        :param epoch: Current epoch number
+        :param logs:
+        :return:
+        """
+        epoch += 1  # start by index 1
+        self.epoch = epoch
+        if not self.eval_on_epochs:
+            return
+        if epoch < self.start_eval_on_epoch:
+            if self.verbose > 0:
+                logging.info('Not evaluating until end of epoch ' + str(self.start_eval_on_epoch))
+            return
+        elif (epoch - self.start_eval_on_epoch) % self.each_n_epochs != 0:
+            if self.verbose > 0:
+                logging.info('Evaluating only every ' + str(self.each_n_epochs) + ' epochs')
+            return
+        self.evaluate(epoch, counter_name='epoch')
+
+    def on_batch_end(self, n_update, logs={}):
+        self.cum_update += 1  # start by index 1
+        if self.eval_on_epochs:
+            return
+        if self.cum_update % self.each_n_epochs != 0:
+            return
+        if self.epoch < self.start_eval_on_epoch:
+            return
+        self.evaluate(self.cum_update, counter_name='update')
+
+    def evaluate(self, epoch, counter_name='epoch'):
+        # Evaluate on each set separately
+        for s in self.set_name:
+            # Apply model predictions
+            params_prediction = {'batch_size': self.batch_size,
+                                 'n_parallel_loaders': self.extra_vars['n_parallel_loaders'],
+                                 'predict_on_sets': [s],
+                                 'pos_unk': False, 'heuristic': 0, 'mapping': None}
+
+            if self.beam_search:
+                params_prediction.update(checkDefaultParamsBeamSearch(self.extra_vars))
+                predictions = self.model_to_eval.predictBeamSearchNet(self.ds, params_prediction)[s]
+            else:
+                # Convert predictions
+                postprocess_fun = None
+                if (self.is_3DLabel):
+                    postprocess_fun = [self.ds.convert_3DLabels_to_bboxes, self.extra_vars[s]['references_orig_sizes']]
+                predictions = \
+                    self.model_to_eval.predictNet(self.ds, params_prediction, postprocess_fun=postprocess_fun)[s]
+
+            if (self.is_text):
+                if params_prediction['pos_unk']:
+                    samples = predictions[0]
+                    alphas = predictions[1]
+
+                    if eval('self.ds.loaded_raw_' + s + '[0]'):
+                        sources = predictions[2]
+                    else:
+                        sources = []
+                        for preds in predictions[2]:
+                            for src in preds[self.input_text_id]:
+                                sources.append(src)
+                        sources = self.model_to_eval.decode_predictions_beam_search(sources,
+                                                                                    self.index2word_x,
+                                                                                    pad_sequences=True,
+                                                                                    verbose=self.verbose)
+                    heuristic = params_prediction['heuristic']
+                else:
+                    samples = predictions
+                    alphas = None
+                    heuristic = None
+                    sources = None
+                if self.out_pred_idx is not None:
+                    samples = samples[self.out_pred_idx]
+                # Convert predictions into sentences
+                if self.beam_search:
+                    predictions = self.model_to_eval.decode_predictions_beam_search(samples,
+                                                                                    self.index2word_y,
+                                                                                    alphas=alphas,
+                                                                                    x_text=sources,
+                                                                                    heuristic=heuristic,
+                                                                                    mapping=
+                                                                                    params_prediction[
+                                                                                        'mapping'],
+                                                                                    verbose=self.verbose)
+                else:
+                    predictions = self.model_to_eval.decode_predictions(predictions, 1,
+                                                                        # always set temperature to 1
+                                                                        self.index2word_y,
+                                                                        self.sampling_type,
+                                                                        verbose=self.verbose)
+
+            # Store predictions
+            if self.write_samples:
+                # Store result
+                filepath = self.save_path + '/' + s + '_' + counter_name + '_' + str(
+                    epoch) + '.pred'  # results file
+                if self.write_type == 'list':
+                    list2file(filepath, predictions)
+                elif self.write_type == 'vqa':
+                    list2vqa(filepath, predictions, self.extra_vars[s]['question_ids'])
+                elif self.write_type == 'listoflists':
+                    listoflists2file(filepath, predictions)
+                elif self.write_type == 'numpy':
+                    numpy2file(filepath, predictions)
+                elif self.write_type == '3DLabels':
+                    # TODO:
+                    print("WRITE SAMPLES FUNCTION NOT IMPLEMENTED")
+                else:
+                    raise NotImplementedError(
+                        'The store type "' + self.write_type + '" is not implemented.')
+
+            # Evaluate on each metric
+            for metric in self.metric_name:
+                if self.verbose > 0:
+                    logging.info('Evaluating on metric ' + metric)
+                filepath = self.save_path + '/' + s + '.' + metric  # results file
+
+                # Evaluate on the chosen metric
+                metrics = evaluation.select[metric](
+                    pred_list=predictions,
+                    verbose=self.verbose,
+                    extra_vars=self.extra_vars,
+                    split=s)
+
+                # Print results to file
+                with open(filepath, 'a') as f:
+                    header = counter_name + ','
+                    line = str(epoch) + ','
+                    for metric_ in sorted(metrics):
+                        value = metrics[metric_]
+                        header += metric_ + ', '
+                        line += str(value) + ', '
+                    if epoch == 1 or epoch == self.start_eval_on_epoch:
+                        f.write(header + '\n')
+                    f.write(line + '\n')
+                if self.verbose > 0:
+                    logging.info('Done evaluating on metric ' + metric)
+
+            # Early stop check
+            if self.early_stop and s in ['val', 'validation', 'dev', 'development']:
+                current_score = metrics[self.stop_metric]
+                if current_score > self.best_score:
+                    self.best_score = current_score
+                    self.best_epoch = epoch
+                    self.wait = 0
+                    if self.verbose > 0:
+                        logging.info(
+                            '---current best %s accuracy: %.4f' % (self.stop_metric, current_score))
+                else:
+                    if self.wait >= self.patience:
+                        if self.verbose > 0:
+                            logging.info(
+                                "%s %d: early stopping. Best %s value found at %s %d: %.4f" %
+                                (str(counter_name), epoch, self.stop_metric, str(counter_name),
+                                 self.best_epoch, self.best_score))
                             self.model.stop_training = True
                     self.wait += 1
                     if self.verbose > 0:
@@ -458,7 +691,7 @@ class SampleEachNUpdates(KerasCallback):
 
             if self.beam_search:
                 params_prediction.update(checkDefaultParamsBeamSearch(self.extra_vars))
-                predictions, truths, data = self.model_to_eval.BeamSearchNet(self.ds, params_prediction)
+                predictions, truths, data = self.model_to_eval.predictBeamSearchNet(self.ds, params_prediction)
             else:
                 raise NotImplementedError()
 
