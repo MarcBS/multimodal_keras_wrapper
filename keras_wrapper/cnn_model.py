e907a9f88c1280269cabe81fa76addd23f7d15c3
MarcBS <marc.bolanos@ub.edu>
2017-01-24 16:19:02 +0100

    Fix DenseNet layers considering original implementation

--- keras_wrapper/cnn_model.py
+++ keras_wrapper/cnn_model.py
@@ -3002,9 +3002,9 @@ class Model_Wrapper(object):
     #       DENSE NETS
     ##############################
 
-    def add_dense_block(self, in_layer, nb_layers, k=12, drop=0.2):
+    def add_up_dense_block(self, in_layer, nb_layers, k=12, drop=0.2):
         """
-        Adds a Dense Block.
+        Adds a Dense Block for the transition down path.
 
         # References
             Jegou S, Drozdzal M, Vazquez D, Romero A, Bengio Y.
@@ -3026,6 +3026,29 @@ class Model_Wrapper(object):
             # Merge with previous layer
             prev_layer = merge([new_layer, prev_layer], mode='concat', concat_axis=1)
         return merge(list_outputs, mode='concat', concat_axis=1)
+
+    def add_down_dense_block(self, in_layer, nb_layers, k=12, drop=0.2):
+        """
+        Adds a Dense Block for the transition up path.
+
+        # References
+            Jegou S, Drozdzal M, Vazquez D, Romero A, Bengio Y.
+            The One Hundred Layers Tiramisu: Fully Convolutional DenseNets for Semantic Segmentation.
+            arXiv preprint arXiv:1611.09326. 2016 Nov 28.
+
+        :param in_layer: input layer to the dense block.
+        :param nb_layers: number of dense layers included in the dense block (see self.add_dense_layer() for information about the internal layers).
+        :param k: growth rate. Number of additional feature maps learned at each layer.
+        :param drop: dropout rate.
+        :return: output layer of the dense block
+        """
+        prev_layer = in_layer
+        for n in range(nb_layers):
+            # Insert dense layer
+            new_layer = self.add_dense_layer(prev_layer, k, drop)
+            # Merge with previous layer
+            prev_layer = merge([new_layer, prev_layer], mode='concat', concat_axis=1)
+        return prev_layer
 
 
     def add_dense_layer(self, in_layer, k, drop):
@@ -3077,10 +3100,11 @@ class Model_Wrapper(object):
         :return: [output layer, skip connection name]
         """
         # Dense Block
-        x_dense = self.add_dense_block(x, nb_layers, k=growth, drop=drop)  # (growth*nb_layers) feature maps added
+        x_dense = self.add_down_dense_block(x, nb_layers, k=growth, drop=drop)  # (growth*nb_layers) feature maps added
 
-        # Concatenation and skip connection recovery for upsampling path
-        skip = merge([x, x_dense], mode='concat', concat_axis=1, name='down_skip_'+str(skip_dim))
+        ## Concatenation and skip connection recovery for upsampling path
+        #skip = merge([x, x_dense], mode='concat', concat_axis=1, name='down_skip_'+str(skip_dim))
+        skip = x_dense
 
         # Transition Down
         x_out = BatchNormalization()(skip)
@@ -3129,7 +3153,7 @@ class Model_Wrapper(object):
             skip = skip_conn[skip_conn_shapes.index(out_dim)]
             x = merge([skip, x], mode='concat', concat_axis=1, name='skip_'+str(out_dim))
         # Dense Block
-        x = self.add_dense_block(x, nb_layers, k=growth, drop=drop)  # (growth*nb_layers) feature maps added
+        x = self.add_up_dense_block(x, nb_layers, k=growth, drop=drop)  # (growth*nb_layers) feature maps added
         return x
 
     def Empty(self, nOutput, input):
