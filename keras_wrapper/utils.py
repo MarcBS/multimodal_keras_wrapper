5b18b5fdeece522875d302d0bb619bdd4a8cc66d
lvapeab <lvapeab@gmail.com>
2018-02-14 16:52:40 +0100

    Avoid importing Keras if unnecessary (in utils.py and dataset.py)

--- keras_wrapper/utils.py
+++ keras_wrapper/utils.py
@@ -615,6 +615,35 @@ def indices_2_one_hot(indices, n):
     return one_hot
 
 
+# From keras.utils.np_utils
+def to_categorical(y, num_classes=None):
+    """Converts a class vector (integers) to binary class matrix.
+
+    E.g. for use with categorical_crossentropy.
+
+    # Arguments
+        y: class vector to be converted into a matrix
+            (integers from 0 to num_classes).
+        num_classes: total number of classes.
+
+    # Returns
+        A binary matrix representation of the input.
+    """
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes))
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
+
 # ------------------------------------------------------- #
 #       DECODING FUNCTIONS
 #           Functions for decoding predictions
